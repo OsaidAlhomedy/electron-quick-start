539787cf806a851f4e07d05fabcc8ebb47c222eb
OsaidAlhomedy <o.alhomedy@gmail.com>
2022-04-26 15:11:53 +0300

    basics

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const { app } = require("electron");
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 app.on("ready", () => {
   console.log("App is running");
+  const mainWindow = new BrowserWindow({});
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 });
